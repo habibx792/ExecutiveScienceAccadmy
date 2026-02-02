97b3ebd7429d55318ceb9eb44e1908ccdeb25360
ghulamhabib364 <ghulamhabib.x@gmail.com>
2026-02-02 09:47:42 +0500

    pix the ui of main form

--- ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/DashBoadMangement/DashBoardForm.cs
+++ ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/DashBoadMangement/DashBoardForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExecutiveScienceAcademy.classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,29 @@ namespace ExecutiveSceinceAccadmy.DashBoadMangement
 {
     public partial class DashBoardForm : Form
     {
+
         public DashBoardForm()
         {
             InitializeComponent();
+            UI.Instance.StyleForm(this,
+                backgroundColor: Color.FromArgb(245, 245, 245),
+                borderRadius: 25,
+                showCustomTitleBar: true,
+                title: "Executive Science Academy");
+
+            StyleControls();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void StyleControls()
+        {
+            UI.Instance.setFormSize(this, 1373, 687);
+
+            UI.Instance.AddFormShadow(this);
+        }
+
+        private void DashBoardForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/ResultManagement/resultFrom.Designer.cs
+++ ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/ResultManagement/resultFrom.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "resultFrom";
+            SuspendLayout();
+            // 
+            // resultFrom
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "resultFrom";
+            Text = "resultFrom";
+            Load += resultFrom_Load;
+            ResumeLayout(false);
         }
 
         #endregion
