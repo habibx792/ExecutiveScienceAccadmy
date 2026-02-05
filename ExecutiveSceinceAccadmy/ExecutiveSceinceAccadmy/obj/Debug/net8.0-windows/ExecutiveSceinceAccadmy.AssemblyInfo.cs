f0a1af9017a818d4b4827703de8fea3131c0e3bc
Ghulam Habib <ghulamhabib.x@gamil.com>
2026-02-05 23:22:21 +0500

    bug fix

--- ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/obj/Debug/net8.0-windows/ExecutiveSceinceAccadmy.AssemblyInfo.cs
+++ ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/obj/Debug/net8.0-windows/ExecutiveSceinceAccadmy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExecutiveSceinceAccadmy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfc6134d481a330edd629229e66f61e3e061ba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c1e178522c49d6217dcc95ea0008b3d3d03c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExecutiveSceinceAccadmy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExecutiveSceinceAccadmy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
