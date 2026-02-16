59922df1c44fe5a8247e22166dd6826128e7243a
Habib <ghulamhabib161@gmail.com>
2026-02-16 23:39:15 +0500

    define general scema of accamy db

--- ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/obj/Debug/net8.0-windows/ExecutiveSceinceAccadmy.AssemblyInfo.cs
+++ ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/obj/Debug/net8.0-windows/ExecutiveSceinceAccadmy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExecutiveSceinceAccadmy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17924d719e8dd9224662b493d82ea0e8a7b28387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0043e129ea400f0d85202e0a4b92db26733a149b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExecutiveSceinceAccadmy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExecutiveSceinceAccadmy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
