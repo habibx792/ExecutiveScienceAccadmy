a2bffdeb1213a9bdfd6d168c0c03cb691dbfe71f
Habib <ghulamhabib.x@gmail.com>
2026-04-23 23:52:48 +0500

    fix size error of registeration form

--- ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/RegisTrationForms/StudentRegistration/RegisTracionForm.Designer.cs
+++ ExecutiveSceinceAccadmy/ExecutiveSceinceAccadmy/RegisTrationForms/StudentRegistration/RegisTracionForm.Designer.cs
@@ -308,7 +308,7 @@
             pnMainPn.Controls.Add(btnRegistation);
             pnMainPn.Location = new Point(22, 95);
             pnMainPn.Name = "pnMainPn";
-            pnMainPn.Size = new Size(1228, 743);
+            pnMainPn.Size = new Size(1228, 783);
             pnMainPn.TabIndex = 26;
             pnMainPn.Paint += pnMainPn_Paint;
             // 
@@ -749,7 +749,7 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.RoyalBlue;
-            ClientSize = new Size(1272, 808);
+            ClientSize = new Size(1272, 890);
             Controls.Add(pnHide);
             Controls.Add(pnMainPn);
             Controls.Add(pnLogo);
